baa3ba7bdcf976fd463ab8e2bfb29cb682164ef7
Liang Jun <phualiangjun@gmail.com>
2021-11-28 20:37:58 +0800

    move stuff out of index

---
+++ helpers/price.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const getLatestPrice = (priceData) => priceData.data.last;
+
+const getEthPrices = async () => {
+  try {
+    return await axios.get("https://www.bitstamp.net/api/v2/ticker/ethusd/");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getBtcPrices = async () => {
+  try {
+    return await axios.get("https://www.bitstamp.net/api/v2/ticker/btcusd/");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const calculateBalance = async (userBalance) => {
+  let balance = 0;
+
+  if (userBalance.ETH) {
+    const ethData = await getEthPrices();
+    balance += getLatestPrice(ethData) * userBalance.ETH;
+  }
+
+  if (userBalance.BTC) {
+    const btcData = await getBtcPrices();
+    balance += getLatestPrice(btcData) * userBalance.BTC;
+  }
+
+  return balance;
+};
--- index.js
+++ index.js
@@ -1,47 +1,12 @@
 import express from "express";
-import axios from "axios";
+import users from "./models/users.js";
+import { calculateBalance } from "./helpers/price.js";
+
 const app = express();
 const port = 3000;
 
-const userBalances = {
-  "user-1": {
-    BTC: "0.5",
-    ETH: "2",
-  },
-  "user-2": {
-    BTC: "0.1",
-  },
-  "user-3": {
-    ETH: "5",
-  },
-};
-
-const getLatestPrice = (priceData) => priceData.data.last;
-
-const calculateBalance = async (userBalance) => {
-  let balance = 0;
-
-  if (userBalance.ETH) {
-    const ethData = await axios.get(
-      "https://www.bitstamp.net/api/v2/ticker/ethusd/"
-    );
-
-    balance += getLatestPrice(ethData) * userBalance.ETH;
-  }
-
-  if (userBalance.BTC) {
-    const btcData = await axios.get(
-      "https://www.bitstamp.net/api/v2/ticker/btcusd/"
-    );
-
-    balance += getLatestPrice(btcData) * userBalance.BTC;
-  }
-
-  return balance;
-};
-
 app.get("/user/:userId/balances", async (req, res) => {
-  const userBalance = userBalances[req.params.userId];
+  const userBalance = users[req.params.userId];
 
   if (userBalance) {
     const balances = {
