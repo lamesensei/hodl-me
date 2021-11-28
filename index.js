56e138672bf844fe23e5dc1b42b5310859b72872
Liang Jun <phualiangjun@gmail.com>
2021-11-28 19:48:13 +0800

    add basic retrieval

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-import express from "express"
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const userBalances = {
+  "user-1": {
+    BTC: "0.5",
+    ETH: "2",
+  },
+  "user-2": {
+    BTC: "0.1",
+  },
+  "user-3": {
+    ETH: "5",
+  },
+};
+
+app.get("/user/:userId/balances", (req, res) => {
+  const user = userBalances[req.params.userId];
+  if (user) {
+    res.json(user);
+  }
+  res.send("No user");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
